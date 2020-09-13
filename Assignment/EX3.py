faf655fd5fd9a180db129b07d78b161f17ac78bd
Vasile <verejanvasile@gmail.com>
2020-09-13 20:46:19 +0100

    Merging to list exampe

---
+++ Assignment/EX3.py
@@ -0,0 +1,12 @@
+"""
+Create a  function to merge two lists and return the result.
+"""
+
+
+def merge_lsits(list_a, list_b):
+    return list_a + list_b
+
+
+list_a = [1, 2, 3, 4]
+list_b = [5, 6, 7, 8]
+print(merge_lsits(list_a, list_b))
